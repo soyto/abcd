6c240f823ddd68362cccc5ce41bb9a8ee83cb224
soyto <alf.ixos@gmail.com>
2017-12-04 17:07:48 +0100

    (DEV) Spelling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 /* global responsiveVoice */
 $(() => {
 
+  const CURRENT_LETTER_CLASS_NAME = 'current-letter';
+
   var _inputText = document.getElementsByClassName('input-text')[0];
   var _playButton = document.getElementsByClassName('play-button')[0];
+  var _textResult = document.getElementsByClassName('result-text')[0];
 
   _inputText.focus();
 
@@ -22,41 +25,81 @@ $(() => {
   function _play() {
     var _text = _inputText.value;
 
+    _removeAllChildNodes(_textResult);
+
+    _text.split('').forEach($$character => {
+      var _spanNode = document.createElement('span');
+      _spanNode.textContent = $$character;
+
+      _textResult.append(_spanNode);
+    });
+
     var $$p = Promise.resolve();
 
     $$p = $$p.then(() => _playText(_text));
 
-    _text.split('').forEach($$character => {
-      if($$character != ' ') {
-        $$p = $$p.then(() => _playText($$character));
-      }
+    _text.split('').forEach( ($$character, $$index) => {
+
+      $$p = $$p.then(() => {
+
+        console.log($$index);
+
+        if($$index > 0) {
+          _textResult.children[$$index - 1].classList.remove(CURRENT_LETTER_CLASS_NAME);
+        }
+
+        _textResult.children[$$index].classList.add(CURRENT_LETTER_CLASS_NAME);
+
+        if($$character != ' ') {
+          return _playText($$character);
+        }
+        else {
+          return Promise.resolve();
+        }
+
+      });
+
+
     });
 
-    $$p = $$p.then(() => _playText(_text));
+    $$p = $$p.then(() => {
+
+      _textResult.children[_textResult.children.length - 1].classList.remove(CURRENT_LETTER_CLASS_NAME)
+      return _playText(_text);
+    });
   }
 
-
+  //Play this text and return a promise when is finished.. (Or hope so)
   function _playText(text) {
     var _utterance = new SpeechSynthesisUtterance(text);
+    var _hasFinished = false;
 
     var $$promise = new Promise(($$resolve, $$reject) => {
 
       _utterance.onend = ($$evt) => {
+        _hasFinished = true;
         console.log('[%s] stopped {%o}', text, $$evt);
         $$resolve();
       };
 
       _utterance.onerror = ($$evt) => {
+        _hasFinished = true;
         console.log('[%s] error {%o}', text, $$evt);
         $$resolve();
       };
 
-      setTimeout(() => $$resolve(), 2000);
+      setTimeout(() => { if(!_hasFinished) { $$resolve(); } }, 2000);
     });
 
     console.log('playing [%s]', text);
     window.speechSynthesis.speak(_utterance);
 
     return $$promise;
+  }
+
+  function _removeAllChildNodes(node) {
+    while(node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
   }
 });
