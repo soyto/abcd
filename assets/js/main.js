c689b890b7cad58062af89427e7ebe0b5d23d237
soyto <alf.ixos@gmail.com>
2017-12-04 16:23:03 +0100

    (DEV) Spelling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,62 @@
 /* global responsiveVoice */
 $(() => {
 
-  responsiveVoice.speak('Killersamus', 'US English Female', {
-    'pitch': 1,
-    'onend': () => responsiveVoice.speak('es una pasada de hombre... que no! es un polluelo', 'Spanish Female')
+  var _inputText = document.getElementsByClassName('input-text')[0];
+  var _playButton = document.getElementsByClassName('play-button')[0];
+
+  _inputText.focus();
+
+  _inputText.addEventListener('blur', () => {
+    _inputText.focus();
   });
+
+  _inputText.addEventListener('keydown', $$evt => {
+    if($$evt['keyCode'] === 13) {
+      _play();
+    }
+  });
+
+  _playButton.addEventListener('click', _play);
+
+
+  function _play() {
+    var _text = _inputText.value;
+
+    var $$p = Promise.resolve();
+
+    $$p = $$p.then(() => _playText(_text));
+
+    _text.split('').forEach($$character => {
+      if($$character != ' ') {
+        $$p = $$p.then(() => _playText($$character));
+      }
+    });
+
+    $$p = $$p.then(() => _playText(_text));
+  }
+
+
+  function _playText(text) {
+    var _utterance = new SpeechSynthesisUtterance(text);
+
+    var $$promise = new Promise(($$resolve, $$reject) => {
+
+      _utterance.onend = ($$evt) => {
+        console.log('[%s] stopped {%o}', text, $$evt);
+        $$resolve();
+      };
+
+      _utterance.onerror = ($$evt) => {
+        console.log('[%s] error {%o}', text, $$evt);
+        $$resolve();
+      };
+
+      setTimeout(() => $$resolve(), 2000);
+    });
+
+    console.log('playing [%s]', text);
+    window.speechSynthesis.speak(_utterance);
+
+    return $$promise;
+  }
 });
